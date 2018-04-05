945e1aa135a6ee2ac0714aeec3b2c6ca7817a2df
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-05 16:05:25 +0100

    More tests passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
   elsif number % 3 == 0 then 'fizz'
-  else
-    'buzz'
+  elsif number % 5 == 0 then 'buzz'
+  else number
   end
 end
