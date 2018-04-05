c8b6d4619c14e318d1888df20c03d6aea02a09f1
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-05 16:07:46 +0100

    passed all tests, program functional

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
-  elsif number % 3 == 0 then 'fizz'
-  elsif number % 5 == 0 then 'buzz'
-  else number
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0 then 'fizzbuzz'
+  elsif num % 3 == 0 then 'fizz'
+  elsif num % 5 == 0 then 'buzz'
+  else num
   end
 end
